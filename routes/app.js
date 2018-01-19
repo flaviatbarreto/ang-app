e58c51fe326ac2dd1e498c08270b96cb6cb63f77
Flávia Barreto <flavia.t.barreto@gmail.com>
2018-01-18 22:03:11 -0300

    Adding message component

--- routes/app.js
+++ routes/app.js
@@ -1,27 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc){
-        if(err)
-        {
-            return res.send('Error!');
-        }
-
-        res.render('message', {name: doc.firstName});
-    });
-});
-
-router.post('/', function (req, res, next) {
-    var user = new User({
-        firstName: 'Flávia',
-        lastName: 'Barreto',
-        password: '123',
-        email: req.body.email
-    });
-    user.save();
-    res.redirect('/');
+    res.render('index');
 });
 
 module.exports = router;
