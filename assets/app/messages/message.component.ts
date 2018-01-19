423f1e7f1e877a36fd60cdad3a492c82114472e5
Flavia Barreto <flavia.barreto@defensoria.ba.def.br>
2018-01-19 13:28:39 -0300

    Add ang click event

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+import { Message } from './message.model'
 @Component({
     selector: 'app-message',
     templateUrl: './message.component.html',
@@ -20,5 +21,10 @@ import { Component } from '@angular/core';
 })
 
 export class MessageComponent {
+    @Input() message: Message;
+    @Output() editClicked = new EventEmitter<string>();
 
+    onEdit() {
+        this.editClicked.emit('a new value');
+    }
 }
