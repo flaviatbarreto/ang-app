360a39b087105b5018a2dfe76b7698fce83b9f4c
Flávia Barreto <flavia.t.barreto@gmail.com>
2018-04-25 22:12:10 -0300

    deleting messages from the database

--- routes/messages.js
+++ routes/messages.js
@@ -73,4 +73,36 @@ router.patch('/:id', function(req, res, next){
     })
 })
 
+router.delete('/:id', (req, res, next) => {
+    Message.findById(req.params.id, function(err, message){
+        if(err){
+            return res.status(500).json({
+                title: "An error occurred",
+                error: err
+            })
+        }
+
+        if(!message){
+            return res.status(500).json({
+                title: "No message found",
+                error: {message: "Message not found"}
+            })
+        }
+
+        message.remove(function(err, result){
+            if(err){
+                return res.status(500).json({
+                    title: "An error occurred",
+                    error: err
+                })
+            }
+
+            res.status(200).json({
+                message: "Message deleted successfully",
+                obj: result
+            })
+        })
+    })
+})
+
 module.exports = router;
