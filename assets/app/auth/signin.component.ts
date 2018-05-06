b64d10a0c157c850bb2f0e1c32a024927a5ec171
Flávia Barreto <flavia.t.barreto@gmail.com>
2018-05-06 20:47:18 -0300

    add user signin

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,23 +1,37 @@
 import { Component, OnInit } from "@angular/core"
 import { FormGroup, FormControl, Validators } from "@angular/forms"
+import { Router } from "@angular/router"
+
+import { User } from './user.model'
+import { AuthService } from './auth.service'
 
 @Component({
     selector: "app-signin",
     templateUrl: "./signin.component.html"
 })
-
 export class SigninComponent {
     myForm: FormGroup
+
+    constructor(private authService: AuthService, private router: Router) {}
+
+    onSubmit() {
+        const user = new User(this.myForm.value.email, this.myForm.value.password)
+        this.authService.signin(user)
+            .subscribe(
+                data => {
+                    localStorage.setItem('token', data.token)
+                    localStorage.setItem('userID', data.userID)
+                    this.router.navigateByUrl('/')
+                },
+                err => console.error(err)
+            )
+        this.myForm.reset()
+    }
 
     ngOnInit() {
         this.myForm = new FormGroup({
             email: new FormControl(null, [Validators.required]),
             password: new FormControl(null, Validators.required)
         })
-    }
-
-    onSubmit() {
-        console.log(this.myForm)
-        this.myForm.reset()
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 var User = require('../models/user');
 
@@ -25,6 +26,39 @@ router.post('/', function (req, res, next) {
             obj: result
         })
     })
+});
+
+router.post('/signin', (req, res, next) => {
+    User.findOne({email: req.body.email}, (err, user) => {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            })
+        }
+
+        if (!user) {
+            return res.status(401).json({
+                title: 'Login failed',
+                error: {message: 'Invalid login credentials'}
+            })
+        }
+
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
+            return res.status(401).json({
+                title: 'Login failed',
+                error: {message: 'Invalid login credentials'}
+            })
+        }
+
+        var token = jwt.sign({user: user}, 'scret123', {expiresIn: 7200});
+
+        res.status(200).json({
+            message: 'Succesfully logged in',
+            token: token,
+            userID: user._id
+        })
+    });
 });
 
 module.exports = router;
