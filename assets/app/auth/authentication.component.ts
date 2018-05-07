c8608d787032d6c35804b8e4e41e76982e11423b
Flávia Barreto <flavia.t.barreto@gmail.com>
2018-05-06 21:35:02 -0300

    change tabs by login status

--- assets/app/auth/authentication.component.ts
+++ assets/app/auth/authentication.component.ts
@@ -1,18 +1,20 @@
 import { Component } from "@angular/core"
 
+import { AuthService } from "./auth.service"
+
 @Component({
     selector: "app-authentication",
     template: `
         <header class="row spacing">
             <nav class="col-md-8 col-md-offset-2">
                 <ul class="nav nav-tabs">
-                    <li routerLinkActive="active">
+                    <li routerLinkActive="active" *ngIf="!isLoggedIn()">
                         <a [routerLink]="['signup']">Signup</a>
                     </li>
-                    <li routerLinkActive="active">
+                    <li routerLinkActive="active" *ngIf="!isLoggedIn()">
                         <a [routerLink]="['signin']">Signin</a>
                     </li>
-                    <li routerLinkActive="active">
+                    <li routerLinkActive="active" *ngIf="isLoggedIn()">
                         <a [routerLink]="['logout']">Logout</a>
                     </li>
                 </ul>
@@ -23,7 +25,10 @@ import { Component } from "@angular/core"
         </div>
     `
 })
-
 export class AuthenticationComponent {
+    constructor(private authService: AuthService) {}
 
+    isLoggedIn() {
+        return this.authService.isLoggedIn();
+    }
 }
